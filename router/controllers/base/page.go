0a876af0953daa5b10e4eb45d9620fa3716183c3
tongyiming <1290863722@qq.com>
2018-08-19 16:40:44 +0800

    init

--- router/controllers/base/page.go
+++ router/controllers/base/page.go
@@ -1,7 +1,21 @@
 package base
 
 type Request struct {
-
+	PageSize int `json:"pageSize"`
+	PageNo int `json:"pageNo"`
+	//DESC or ASC
+	Sort string `json:"sort"`
 }
+
+
+type Response struct {
+	CurPage int `json:"curPage"`
+	PageSize int `json:"pageSize"`
+	TotalSize int `json:"totalSize"`
+	TotalPage int `json:"totalPage"`
+	FirstPage bool `json:"firstPage"`
+	LastPage bool `json:"lastPage"`
+}
+
 
 
